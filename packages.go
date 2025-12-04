edc8805ba0d1f99ffe5f33c4d23f0d711d2729c8
leeka <kanonlee207@gmail.com>
2025-12-04 12:06:52 +0000

    fix file pkg

--- packages.go
+++ packages.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"golang.org/x/tools/go/packages"
 )
@@ -9,14 +10,15 @@ import (
 func packageForFile(file string) (string, error) {
 	cfg := &packages.Config{
 		Mode: packages.NeedName | packages.NeedFiles | packages.NeedModule,
+		Dir:  filepath.Dir(file),
 	}
-	pkgs, err := packages.Load(cfg, "file="+file)
+	pkgs, err := packages.Load(cfg, "file="+filepath.Base(file))
 	if err != nil {
 		return "", err
 	}
 	if len(pkgs) == 0 {
 		return "", fmt.Errorf("no package found for %s", file)
 	}
-	// fmt.Println("file pkg", file, pkgs)
+
 	return pkgs[0].PkgPath, nil
 }
